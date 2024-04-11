26decc0cc73efb50f046cc0c831176abe91e7c9d
KavishkaSasindu <sasindu0215@gmail.com>
2024-04-11 22:07:50 +0530

    updated title

--- src/app/modules/security/components/log-in/log-in.component.ts
+++ src/app/modules/security/components/log-in/log-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from "@angular/router";
 import {AuthServiceService} from "../../../shared/services/auth-service.service";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-log-in',
@@ -12,14 +13,17 @@ export class LogInComponent implements OnInit {
   email:string ="";
   password:string = "";
 
-  constructor(private router:Router,private authService:AuthServiceService) { }
+  constructor(private router:Router,private authService:AuthServiceService,private title:Title) { }
 
   ngOnInit(): void {
 
     if(this.authService.isExistToken('my-token')){
+      this.title.setTitle('Console | Dashboard ');
       this.router.navigateByUrl('/console').then(()=>{
         console.log(true)
       })
+    }else{
+      this.title.setTitle('Security | logIn')
     }
   }
 
